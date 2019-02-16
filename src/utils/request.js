bae6d676b44d47c606fdc9c8bd910a6eb4754118
Sauce <550275752@qq.com>
2019-02-16 22:53:09 +0800

    axios response拦截处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,7 @@ import store from '~/store';
 import { message } from 'simple-ui-for-vue';
 import { getToken } from '~/uils/getCookie';
 
+const apiDebug = true;
 
 const service = axios.create({
 	base: process.env.BASE_API,         //	config/dev.env.js中配置地址
@@ -14,8 +15,18 @@ service.interceptors.request.use(config => {
 	//before request
 	config['token'] = getToken();
 	configp['timestrap'] = new Date().getTime();
+	return config;
 }, error => {
 	//request error callback
+	if(apiDebug) console.log('err' + error);
 	store.commit('recordHttp', error);
 	Promise.reject(error)
+})
+
+//添加响应拦截器
+service.interceptors.response.use(response => {
+	//response resultCode问题处理
+}, error => {
+	apiDebug && console.log('err' + error);
+	return Promise.reject(error);
 })
