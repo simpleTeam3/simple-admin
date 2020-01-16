640df6dbc45b3ceadaf797de5c3e9345368c264c
Sauce <550275752@qq.com>
2020-01-16 09:17:36 +0800

    vue cli create

--- config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_API: '"https://www.easy-mock.com/mock/5c6800051b1cdb6835813525/dev"'
-})
- 
--- src/store/mutation-type.js
+++
@@ -1,2 +0,0 @@
-export const RECORD_HTTP = "RECORD_HTTP";
-export const SET_TOKEN = "SET_TOKEN";
